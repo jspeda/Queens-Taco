22ab8c38aad5a924388425f0526c6d6f11e2d089
Joe Spedale <jspedale@gmail.com>
2016-06-04 12:31:50 +0000

    Added comments database and model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :address, presence: true
   validates :description, presence: true
